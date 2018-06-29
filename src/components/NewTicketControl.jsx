d5c965089a53ba9f16db2f6a518b20d384c89f4a
julia sheremet <juliajessicasher@gmail.com>
2018-06-28 17:07:45 -0700

    Add state in the constructor of newticketcontrol

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class NewTicketControl extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      formVisableOnPage: false
+    }
+  }
 
   render(){
     return (
