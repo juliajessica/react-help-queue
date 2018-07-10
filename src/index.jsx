a0706afb189e563ab944fdb07a5ad528a3899c98
julia <juliajessicasher@gmail.com>
2018-07-10 05:46:17 -0700

    Add provider to the entry of the application and import redux

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,20 @@ import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
 
+import { createStore } from 'redux';
+import ticketListReducer from './reducers/ticket-list-reducer';
+import ticketListReducer from './reducers/ticket-list-reducer';
+import { Provider } from 'react-redux'; //provides component tree access to Redux.
+
+const store = createStore(ticketListReducer);
+
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <HashRouter>
-        <Component/>
+        <Provider>
+          <Component/>
+        </Provider>
       </HashRouter>
     </AppContainer>,
     document.getElementById('react-app-root')
